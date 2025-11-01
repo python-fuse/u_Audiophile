ce61e5b1299750792801f94f6ecf9ada50b9f75d
python-fuse <hauwamuktar880@gmail.com>
2025-11-01 11:57:23 +0100

    feat: implemented custom buttons with brand colors

---
+++ components/button.tsx
@@ -0,0 +1,25 @@
+import { ChevronRight } from "lucide-react";
+import React from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  variant?: "primary" | "outlined" | "text-icon";
+  children?: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  className = "",
+  variant = "primary",
+  children,
+  ...props
+}) => {
+  return (
+    <button className={`${className} ${variant} w-40 h-12 `} {...props}>
+      {children}
+
+      {variant == "text-icon" && <ChevronRight className="text-primary " />}
+    </button>
+  );
+};
+
+export default Button;
