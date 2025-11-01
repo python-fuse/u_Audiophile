3117d3d68ab44e4c78ea4d7094e8f3a28a5f894e
python-fuse <hauwamuktar880@gmail.com>
2025-11-01 13:31:08 +0100

    assets: Add earphones page component and image asset for ZX9 speaker

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,14 @@
-"use client";
+import React from "react";
 
-import Button from "@/components/button";
-import NumberInput from "@/components/form/NumberInput";
-import RadioInput from "@/components/form/RadioInput";
-import TextField from "@/components/form/TextField";
-
-const page = () => {
-  return (
-    <>
-      <div className="flex flex-col gap-2 p-7">
-        Testing buttons
-        <Button>See product</Button>
-        <Button variant="outlined">See product</Button>
-        <Button variant="text-icon">Shop</Button>
-      </div>
-
-      <div className="flex flex-col gap-y-4 p-7">
-        Testing form fields
-        <TextField label="Name" value="Umar" onChange={() => {}} />
-        <RadioInput
-          id="option1"
-          isActive={true}
-          label="Option 1"
-          onSelect={() => {}}
-        />
-        <RadioInput
-          id="option1"
-          isActive={false}
-          label="Option 1"
-          onSelect={() => {}}
-        />
-        <NumberInput value={2} onChange={() => {}} min={1} max={5} />
-      </div>
-    </>
-  );
+const Home = () => {
+  // nav bar component
+  // hero section component
+  // categories component
+  // featured speaker1
+  // featured speaker2
+  // featured earphone1
+  // featured earphone2
+  // Footer component
 };
 
-export default page;
+export default Home;
