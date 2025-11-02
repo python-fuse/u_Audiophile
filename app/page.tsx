1e8aaab1665c9ccd9f3eaafc2a944d0ea1896b5b
python-fuse <hauwamuktar880@gmail.com>
2025-11-02 12:05:14 +0100

    feat: categories section +responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,25 @@
+import Categories from "@/components/home/Categories";
 import HeroSection from "@/components/home/Hero";
 import NavBar from "@/components/shared/NavBar";
 
 const Home = () => {
   return (
-    <div className="relative">
+    <div className="relative flex flex-col gap-y-30">
       {/* nav bar component - positioned absolutely over hero */}
       <div className="absolute top-0 left-0 right-0 z-50">
         <NavBar className="bg-black/10" />
       </div>
       {/* hero section component - full viewport height */}
       <HeroSection />
-      {/* categories component */}
-      {/* featured speaker1 */}
-      {/* featured speaker2 */}
-      {/* featured earphone1 */}
-      {/* featured earphone2 */}
-      {/* Footer component */}
+      <section className="container-p">
+        {/* categories component */}
+        <Categories />
+        {/* featured speaker1 */}
+        {/* featured speaker2 */}
+        {/* featured earphone1 */}
+        {/* featured earphone2 */}
+        {/* Footer component */}
+      </section>
     </div>
   );
 };
---
+++ components/home/Categories.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import React from "react";
+import Button from "../button";
+import Link from "next/link";
+import styles from "./category.module.css";
+
+const categoriesData = [
+  {
+    title: "Headphones",
+    imageUrl: "/assets/headphone.png",
+    href: "/headphones",
+  },
+  {
+    title: "Speakers",
+    imageUrl: "/assets/speaker.png",
+    href: "/speakers",
+  },
+  {
+    title: "Earphones",
+    imageUrl: "/assets/earphone.png",
+    href: "/earphones",
+  },
+];
+
+const Categories = () => {
+  return (
+    <div className="flex flex-col md:flex-row justify-center gap-x-[30px] gap-y-[30px] items-center">
+      {categoriesData.map((category) => (
+        <CategoryCard
+          key={category.title}
+          title={category.title}
+          imageUrl={category.imageUrl}
+          href={category.href}
+        />
+      ))}
+    </div>
+  );
+};
+
+interface CategoryCardProps {
+  title: string;
+  imageUrl: string;
+  href: string;
+}
+export const CategoryCard: React.FC<CategoryCardProps> = ({
+  href,
+  imageUrl,
+  title,
+}) => {
+  return (
+    <div
+      className={`${styles["cat-card"]} md:flex-1 w-full md:w-auto h-[350px] md:h-[284px] flex flex-col  md:justify-center items-center gap-y-4`}
+    >
+      <Image src={imageUrl} alt={title} width={122.95} height={160} />
+      <h6>{title}</h6>
+      <Link href={href}>
+        <Button variant="text-icon">Shop</Button>
+      </Link>
+    </div>
+  );
+};
+
+export default Categories;
